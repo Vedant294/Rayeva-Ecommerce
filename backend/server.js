e75f32f4721a55da3ad33629c4d5fd151151039b
Vedant294 <142480046+Vedant294@users.noreply.github.com>
2026-03-14 00:31:22 +0530

    Add database seeding on startup

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
+    const { seedDatabase } = require('./services/dbService');
+    seedDatabase();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
