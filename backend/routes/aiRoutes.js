1bd06f7db517e9d8359305fa52b86b62f9df4d81
Vedant294 <142480046+Vedant294@users.noreply.github.com>
2026-03-14 09:39:36 +0530

    Revert to separate backend deployment

--- backend/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const aiController = require('../controllers/aiController');
-
-router.post('/generate-tags', aiController.generateCategoryAndTags);
-
-module.exports = router;
