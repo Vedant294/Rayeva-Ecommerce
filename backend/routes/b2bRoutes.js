c998732c2fc8613d0250248986f8e7cc3cebf752
Vedant294 <142480046+Vedant294@users.noreply.github.com>
2026-03-14 09:56:20 +0530

    Remove old backend folder

--- backend/routes/b2bRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const b2bController = require('../controllers/b2bController');
-
-router.post('/', b2bController.createB2BProposal);
-router.get('/', b2bController.getB2BProposals);
-
-module.exports = router;
--- backend/routes/impactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const impactController = require('../controllers/impactController');
-
-router.get('/', impactController.getImpactReports);
-router.get('/summary', impactController.getImpactSummary);
-
-module.exports = router;
